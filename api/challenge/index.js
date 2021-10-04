ae21c63bdf31e727f7e4601ae6462debeeeb9e28
Maxim Salnikov <salnikov@gmail.com>
2021-10-04 23:28:07 +0200

    functions

--- api/challenge/index.js
+++ api/challenge/index.js
@@ -8,15 +8,11 @@ module.exports = async function (context, req) {
   try {
     const response = await axios({
       method: 'get',
-      url: `https://docs.microsoft.com/api/lists/${id}`,
+      url: `https://docs.microsoft.com/api/challenges/${id}`,
       json: true,
     });
 
     data = response.data;
-
-    /*     durationInMinutes = response.data.items
-      .filter((e) => e.data.durationInMinutes)
-      .reduce((sum, e) => sum + e.data.durationInMinutes, 0); */
   } catch (err) {
     context.log.error('ERROR', err);
 
@@ -31,10 +27,8 @@ module.exports = async function (context, req) {
 
   context.res = {
     body: {
-      totalDuration: data.totalDuration,
-      moduleCount: data.moduleCount,
-      itemCount: data.itemCount,
-      name: data.name,
+      challengeCollectionId: data.challengeCollectionId,
+      title: data.title,
       description: data.description,
     },
   };
